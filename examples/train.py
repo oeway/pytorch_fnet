5eeb13523e2f6f2c2dd203192f90e047e92d155e
oeway <oeway007@gmail.com>
2019-06-30 23:22:54 -0700

    add train

---
+++ examples/train.py
@@ -0,0 +1,46 @@
+import os
+import json
+
+
+from fnet.cli.init import save_default_train_options
+
+###################################################
+# Download the 3D multi-channel tiffs via Quilt/T4
+###################################################
+
+gpu_id = 0
+
+n_images_to_download = 40  # more images the better
+train_fraction = 0.75
+
+model_save_dir = "{}/model/".format(os.getcwd())
+prefs_save_path = "{}/prefs.json".format(model_save_dir)
+
+
+
+################################################
+# Run the label-free stuff (dont change this)
+################################################
+
+save_default_train_options(prefs_save_path)
+
+with open(prefs_save_path, "r") as fp:
+    prefs = json.load(fp)
+
+prefs["n_iter"] = 50000  # takes about 16 hours, go up to 250,000 for full training
+prefs["interval_checkpoint"] = 10000
+
+prefs["dataset_train"] = "fnet.data.HPAOnlineDataset"
+prefs["dataset_train_kwargs"] = {}
+prefs["dataset_val"] = "fnet.data.HPAOnlineDataset"
+prefs["dataset_val_kwargs"] = {}
+
+# This Fnet call will be updated as a python API becomes available
+
+with open(prefs_save_path, "w") as fp:
+    json.dump(prefs, fp)
+
+command_str = "fnet train {} --gpu_ids {}".format(prefs_save_path, gpu_id)
+
+print(command_str)
+os.system(command_str)
